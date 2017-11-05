2e965bb075339d4e5e30f8d0aa1b0b511c32ca04
jassonX <aceaxiu@gmail.com>
2017-11-05 15:47:07 -0600

    2

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,6 +34,7 @@ end
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
+  config.assets.precompile += %W(rails_admin/rails_admin.css rails_admin/rails_admin.js)
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
