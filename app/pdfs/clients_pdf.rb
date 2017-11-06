bf282dc2c1bcc32fded65793ecd7fe3cfa9ad519
jassonX <aceaxiu@gmail.com>
2017-11-06 15:09:36 -0600

    ed

--- app/pdfs/clients_pdf.rb
+++ app/pdfs/clients_pdf.rb
@@ -19,7 +19,7 @@ class ClientsPdf < Prawn::Document
 	#CABECERA
 
 		def cabecera_rows
-			data = [["\nGRUPO: \ #{@grupo}" ] + [:image => "#{Rails.root}/app/assets/images/ccs.png",  :position => :center]]
+			data = [["\nGRUPO: \ #{@grupo}" "\nNombre y Firma de Capturista:________________"] + [:image => "#{Rails.root}/app/assets/images/ccs.png",  :position => :center]]
 			table data, :cell_style => { :width => 381, :background_color => 'CEF6F5'}
 		end
 	#CABECERA
@@ -32,19 +32,20 @@ class ClientsPdf < Prawn::Document
 	def clients_rows
 
 	  rows = []
-	  rows << ['NOMBRE','DIRECCIÓN', 'NOMBRE AVAL', 'DIRECCION AVAL', 'GARANTIAS', 'P  1', 'P  2', 'P  3', 'P  4', 'P  5', 'P  6', 'P  7', 'P  8', 'P  9', 'P 10', 'P 11', 'P 12', 'P 13', 'P 14', 'P 15' ]
+	  rows << ['NOMBRE','DIRECCIÓN', 'NOMBRE AVAL', 'DIRECCION AVAL', 'GARANTIAS', 'MONTO', 'PAGO', 'P  1', 'P  2', 'P  3', 'P  4', 'P  5', 'P  6', 'P  7', 'P  8', 'P  9', 'P 10', 'P 11', 'P 12', 'P 13', 'P 14', 'P 15' ]
 
 	  @cliente.each do |clt|
 	    @cliente_row = clt.nombre + ' ' + clt.apellidos
 			@dir_row = clt.direccion
+			@monto = clt.monto
 	     clt.avals.map do |avl|
 	      @aval_row = avl.nombre + ' ' + avl.apellidos
 				@dir_row = avl.direccion
 				clt.warranties.map do |gar|
 					@gar_row = gar.garantia1 + ',' + gar.garantia2 + ',' + gar.garantia3 + ',' + gar.garantia4
-				end
 		 end
-	    rows << [@cliente_row,@dir_row, @aval_row, @dir_row, @gar_row, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+	 end
+	    rows << [@cliente_row,@dir_row, @aval_row, @dir_row, @gar_row, @monto, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
 	  end
 	  rows
 	end
